44891c7bc9c6f3d41410fd783a8dba8b104e89e1
huszzzz@163.com <Hu123ashuang>
2020-05-10 19:15:39 +0800

    提交

--- src/main/java/com/catcoder/demo/controller/NacosController.java
+++ src/main/java/com/catcoder/demo/controller/NacosController.java
@@ -1,21 +1,13 @@
 package com.catcoder.demo.controller;
 
-import com.alibaba.nacos.api.annotation.NacosProperties;
-import com.alibaba.nacos.api.config.annotation.NacosValue;
-import com.catcoder.demo.bean.MyLinkTreeNode;
-import com.catcoder.demo.service.ITreeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-
 /**
  * @program: demo
  * @description: tree接口类
