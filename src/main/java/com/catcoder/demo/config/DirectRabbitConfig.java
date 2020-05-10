b4575a5f13800ed0bf082ebef81dc49cdc6347a5
huszzzz@163.com <Hu123ashuang>
2020-05-10 23:15:11 +0800

    提交

--- src/main/java/com/catcoder/demo/config/DirectRabbitConfig.java
+++ src/main/java/com/catcoder/demo/config/DirectRabbitConfig.java
@@ -5,8 +5,10 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class DirectRabbitConfig  extends BaseConfig{
+@Configuration
+public class DirectRabbitConfig{
 
     //队列 起名：TestDirectQueue
     @Bean
